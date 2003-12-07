b65ca438196c2e47c789a615d68d178c1e286a0b
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2003-12-07 17:33:14 +0000

    no message

---
+++ xbt/XBT Manager/XBT Manager.cpp
@@ -0,0 +1,61 @@
+// XBT Manager.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "XBT Manager.h"
+#include "XBT ManagerDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CXBTManagerApp
+
+BEGIN_MESSAGE_MAP(CXBTManagerApp, CWinApp)
+	//{{AFX_MSG_MAP(CXBTManagerApp)
+	//}}AFX_MSG
+	ON_COMMAND(ID_HELP, CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CXBTManagerApp construction
+
+CXBTManagerApp::CXBTManagerApp()
+{
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// The one and only CXBTManagerApp object
+
+CXBTManagerApp theApp;
+
+/////////////////////////////////////////////////////////////////////////////
+// CXBTManagerApp initialization
+
+BOOL CXBTManagerApp::InitInstance()
+{
+	if (!AfxSocketInit())
+	{
+		AfxMessageBox(IDP_SOCKETS_INIT_FAILED);
+		return FALSE;
+	}
+
+	// Standard initialization
+
+#ifdef _AFXDLL
+	Enable3dControls();			// Call this when using MFC in a shared DLL
+#else
+	Enable3dControlsStatic();	// Call this when linking to MFC statically
+#endif
+
+	CXBTManagerDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+
+	// Since the dialog has been closed, return FALSE so that we exit the
+	//  application, rather than start the application's message pump.
+	return FALSE;
+}
---
+++ xbt/XBT Manager/XBT ManagerDlg.h
@@ -0,0 +1,76 @@
+// XBT ManagerDlg.h : header file
+//
+
+#if !defined(AFX_XBTMANAGERDLG_H__7EF696C3_D9AD_467D_8F2E_9DC97F402FCB__INCLUDED_)
+#define AFX_XBTMANAGERDLG_H__7EF696C3_D9AD_467D_8F2E_9DC97F402FCB__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+/////////////////////////////////////////////////////////////////////////////
+// CXBTManagerDlg dialog
+
+class CXBTManagerDlg : public ETSLayoutDialog
+{
+// Construction
+public:
+	int compare(LPARAM lParam1, LPARAM lParam2);
+	void load(const char* name);
+	void save(const char* name);
+	void insert(const string& name);
+	void auto_resize();
+	CXBTManagerDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CXBTManagerDlg)
+	enum { IDD = IDD_XBTMANAGER_DIALOG };
+	CListCtrl	m_list;
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CXBTManagerDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	//{{AFX_MSG(CXBTManagerDlg)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnDropFiles(HDROP hDropInfo);
+	afx_msg void OnGetdispinfoList(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnDblclkList(NMHDR* pNMHDR, LRESULT* pResult);
+	virtual void OnOK();
+	afx_msg void OnColumnclickList(NMHDR* pNMHDR, LRESULT* pResult);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+private:
+	struct t_map_entry
+	{
+		string fname;
+		string info_hash;
+		string name;
+		string tracker;
+		int leechers;
+		int seeders;
+	};
+
+	typedef map<int, t_map_entry> t_map;
+
+	string m_buffer[4];
+	int m_buffer_w;
+	t_map m_map;
+	int m_sort_column;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_XBTMANAGERDLG_H__7EF696C3_D9AD_467D_8F2E_9DC97F402FCB__INCLUDED_)
