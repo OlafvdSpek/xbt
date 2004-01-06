f00eb391fc9608826bc8fda7bccf67b786d7a8ab
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-01-06 16:31:30 +0000

    no message

---
+++ xbt/misc/xcc_z.cpp
@@ -0,0 +1,71 @@
+// xcc_z.cpp: implementation of the xcc_z class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "xcc_z.h"
+
+#include <cstdio>
+#ifdef _MSC_VER
+#include <zlib/zlib.h>
+#else
+#include <zlib.h>
+#endif
+
+Cvirtual_binary xcc_z::gzip(const byte* s, int cb_s)
+{
+	Cvirtual_binary d;
+	unsigned long cb_d = cb_s + (cb_s + 999) / 1000 + 12;
+	byte* w = d.write_start(10 + cb_d + 8);
+	*w++ = 0x1f;
+	*w++ = 0x8b;
+	*w++ = Z_DEFLATED;
+	*w++ = *w++ = *w++ = *w++ = *w++ = *w++ = 0;
+	*w++ = 3;
+	{
+		z_stream stream;
+		stream.zalloc = NULL;
+		stream.zfree = NULL;
+		stream.opaque = NULL;
+		deflateInit2(&stream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, -MAX_WBITS, MAX_MEM_LEVEL, Z_DEFAULT_STRATEGY);
+		stream.next_in = const_cast<byte*>(s);
+		stream.avail_in = cb_s;
+		stream.next_out = w;
+		stream.avail_out = cb_d;
+		deflate(&stream, Z_FINISH);
+		w = stream.next_out;
+	}
+	*reinterpret_cast<int*>(w) = crc32(crc32(0, NULL, 0), s, cb_s);
+	w += 4;
+	*reinterpret_cast<int*>(w) = cb_s;
+	w += 4;
+	d.size(w - d.data());
+	return d;
+}
+
+Cvirtual_binary xcc_z::gzip(const string& v)
+{
+	return gzip(reinterpret_cast<const byte*>(v.c_str()), v.length());
+}
+
+Cvirtual_binary xcc_z::gzip(const Cvirtual_binary& s)
+{
+	return gzip(s, s.size());
+}
+
+void xcc_z::gzip_out(const void* s, int cb_s)
+{
+	gzFile f = gzdopen(fileno(stdout), "wb");
+	gzwrite(f, const_cast<void*>(s), cb_s);
+	gzflush(f, Z_FINISH);
+}
+
+void xcc_z::gzip_out(const string& v)
+{
+	gzip_out(v.c_str(), v.length());
+}
+
+void xcc_z::gzip_out(const Cvirtual_binary& s)
+{
+	gzip_out(s, s.size());
+}
---
+++ xbt/misc/xcc_z.h
@@ -0,0 +1,24 @@
+// xcc_z.h: interface for the xcc_z class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_XCC_Z_H__63B3CD06_15B5_11D6_B606_C89000A7846E__INCLUDED_)
+#define AFX_XCC_Z_H__63B3CD06_15B5_11D6_B606_C89000A7846E__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "virtual_binary.h"
+
+namespace xcc_z
+{
+	Cvirtual_binary gzip(const byte* s, int cb_s);
+	Cvirtual_binary gzip(const string& v);
+	Cvirtual_binary gzip(const Cvirtual_binary&);
+	void gzip_out(const void* s, int cb_s);
+	void gzip_out(const string& v);
+	void gzip_out(const Cvirtual_binary&);
+}
+
+#endif // !defined(AFX_XCC_Z_H__63B3CD06_15B5_11D6_B606_C89000A7846E__INCLUDED_)
