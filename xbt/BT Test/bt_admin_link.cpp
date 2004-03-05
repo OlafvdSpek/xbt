89b10c115cfb71fa29aa5b507d24de9828e722b0
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-03-05 11:20:02 +0000

    no message

--- xbt/BT Test/bt_admin_link.cpp
+++ xbt/BT Test/bt_admin_link.cpp
@@ -5,7 +5,9 @@
 #include "stdafx.h"
 #include "bt_admin_link.h"
 
+#include "bt_strings.h"
 #include "server.h"
+#include "stream_writer.h"
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
@@ -27,10 +29,6 @@ Cbt_admin_link::Cbt_admin_link(Cserver* server, const sockaddr_in& a, const Csoc
 	m_write_b.size(64 << 10);
 }
 
-Cbt_admin_link::~Cbt_admin_link()
-{
-}
-
 int Cbt_admin_link::pre_select(fd_set* fd_read_set, fd_set* fd_write_set, fd_set* fd_except_set)
 {
 	if (m_read_b.cb_w())
@@ -45,14 +43,38 @@ void Cbt_admin_link::post_select(fd_set* fd_read_set, fd_set* fd_write_set, fd_s
 	if (m_read_b.cb_w() && FD_ISSET(m_s, fd_read_set))
 	{
 		recv();
+#if 1
 		strstream str;
-		str << "HTTP/1.0 200\r\ncontent-type: text/html\r\n\r\n";
-		m_server->dump(str);
+		str << "HTTP/1.0 200\r\ncontent-type: text/html\r\n\r\n"
+			<< *m_server;
 		m_write_b.write(str.str(), str.pcount());
 		m_close = true;
+#endif
+		while (1)
+		{
+			while (m_read_b.cb_r() >= 4)
+			{
+				int cb_m = ntohl(*reinterpret_cast<const __int32*>(m_read_b.r()));
+				if (cb_m)
+				{
+					if (m_read_b.cb_r() < 4 + cb_m)
+						break;
+					const char* s = m_read_b.r() + 4;
+					m_read_b.cb_r(4 + cb_m);
+					read_message(s, s + cb_m);
+				}
+				else
+					m_read_b.cb_r(4);
+			}
+			if (m_read_b.cb_r() == m_read_b.cb_read())
+				break;
+			m_read_b.combine();
+		}
 	}
 	if (m_write_b.cb_r() && FD_ISSET(m_s, fd_write_set))
 		send();
+	if (0 && time(NULL) - m_ctime > 60)
+		close();
 }
 
 void Cbt_admin_link::recv()
@@ -111,4 +133,27 @@ void Cbt_admin_link::send()
 void Cbt_admin_link::close()
 {
 	m_s.close();
+}
+
+static byte* write(byte* w, int v)
+{
+	*reinterpret_cast<__int32*>(w) = htonl(v);
+	return w + 4;
+}
+
+void Cbt_admin_link::read_message(const char* r, const char* r_end)
+{
+	switch (*r++)
+	{
+	case bti_get_status:
+		{
+			Cvirtual_binary d;
+			Cstream_writer w(d.write_start(5 + m_server->pre_dump()));
+			w.write_int32(d.size());
+			w.write_int8(bti_status);
+			m_server->dump(w);
+			assert(w.w() == d.data_end());
+		}
+		break;
+	}
 }
