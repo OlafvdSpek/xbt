ec7b8b62a24c4338134bfaffbc9f89e66c547a33
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-03-08 13:04:08 +0000

    no message

--- xbt/misc/bt_torrent.cpp
+++ xbt/misc/bt_torrent.cpp
@@ -36,12 +36,53 @@ int Cbt_torrent::write(const Cvirtual_binary& v)
 int Cbt_torrent::write(const Cbvalue& v)
 {
 	m_announce = v.d(bts_announce).s();
-	write_info(v.d(bts_info));
-	return 0;
+	return write_info(v.d(bts_info));
 }
 
 int Cbt_torrent::write_info(const Cbvalue& v)
 {
+	const Cbvalue::t_list& files = v.d(bts_files).l();
+	for (Cbvalue::t_list::const_iterator i = files.begin(); i != files.end(); i++)
+	{
+		string name;
+		__int64 size = i->d(bts_length).i();
+		{
+			const Cbvalue::t_list& path = i->d(bts_path).l();
+			for (Cbvalue::t_list::const_iterator i = path.begin(); i != path.end(); i++)
+			{
+				if (i->s().empty() || i->s().find_first_of("/\\") != string::npos)
+					return 1;
+				name += '/' + i->s();
+			}
+		}
+		if (name.empty())
+			return 1;
+		m_files.push_back(Cfile(name, size));
+	}
+	if (m_files.empty())
+		m_files.push_back(Cfile("", v.d(bts_length).i()));
 	m_name = v.d(bts_name).s();
+	m_piece_size = v.d(bts_piece_length).i();
 	return 0;
+}
+
+__int64 Cbt_torrent::size() const
+{
+	__int64 r = 0;
+	for (t_files::const_iterator i = m_files.begin(); i != m_files.end(); i++)
+		r += i->size();
+	return r;
+}
+
+bool Cbt_torrent::valid() const
+{
+	for (t_files::const_iterator i = m_files.begin(); i != m_files.end(); i++)
+	{
+		if (i->size() < 1)
+			return false;
+	}
+	return !announce().empty()
+		&& !files().empty()
+		&& piece_size() >= 16 << 10
+		&& piece_size() <= 4 << 20;
 }
--- xbt/misc/bt_torrent.h
+++ xbt/misc/bt_torrent.h
@@ -14,6 +14,37 @@
 class Cbt_torrent  
 {
 public:
+	class Cfile
+	{
+	public:
+		const string& name() const
+		{
+			return m_name;
+		}
+
+		__int64 size() const
+		{
+			return m_size;
+		}
+
+		Cfile()
+		{
+		}
+
+		Cfile(const string& name, __int64 size)
+		{
+			m_name = name;
+			m_size = size;
+		}
+	private:
+		string m_name;
+		__int64 m_size;
+	};
+
+	typedef vector<Cfile> t_files;
+
+	__int64 size() const;
+	bool valid() const;
 	int write(const Cvirtual_binary&);
 	int write(const Cbvalue&);
 	int write_info(const Cbvalue& v);
@@ -25,13 +56,25 @@ public:
 		return m_announce;
 	}
 
+	const t_files& files() const
+	{
+		return m_files;
+	}
+
 	const string& name() const
 	{
 		return m_name;
 	}
+
+	int piece_size() const
+	{
+		return m_piece_size;
+	}
 private:
 	string m_announce;
+	t_files m_files;
 	string m_name;
+	int m_piece_size;
 };
 
 #endif // !defined(AFX_BT_TORRENT_H__AF87B246_788C_42B3_BE1C_08679DFEFBA4__INCLUDED_)
