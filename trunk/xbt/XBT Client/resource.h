f0b0b14a3677b1e0b61812ad5d43ab686e5f9627
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-03-09 23:58:47 +0000

    no message

--- trunk/xbt/XBT Client/resource.h
+++ trunk/xbt/XBT Client/resource.h
@@ -12,13 +12,15 @@
 #define ID_POPUP_OPEN                   32771
 #define ID_POPUP_CLOSE                  32772
 #define ID_POPUP_OPTIONS                32773
+#define ID_POPUP_EXIT                   32774
+#define ID_POPUP_EXPLORE                32775
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           105
 #endif
