99bc4f51b7633a3995f83c5292883976a382e0e8
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-03-22 12:09:41 +0000

    no message

--- xbt/XBT Client/XBT Client.cpp
+++ xbt/XBT Client/XBT Client.cpp
@@ -29,7 +29,6 @@ CXBTClientApp theApp;
 
 BOOL CXBTClientApp::InitInstance()
 {
-	m_server_thread = NULL;
 	CCommandLineInfo cmdInfo;
 	ParseCommandLine(cmdInfo);
 	CreateMutex(NULL, true, "9a6bfda6-7733-4b7d-92b0-3046c9191830");
@@ -55,19 +54,9 @@ BOOL CXBTClientApp::InitInstance()
 #endif
 	_setmaxstdio(2 << 10);
 	SetRegistryKey("XBT");
-	char path[MAX_PATH];
-	if (SUCCEEDED(SHGetSpecialFolderPath(NULL, path, CSIDL_PERSONAL, true)))
-	{
-		strcat(path, "\\XBT");
-		CreateDirectory(path, NULL);
-		m_server.dir(path);
-	}
-	m_server_thread = AfxBeginThread(backend_thread, this);
-	m_server_thread->m_bAutoDelete = false;
 
 	CXBTClientDlg dlg;
 	m_pMainWnd = &dlg;
-	dlg.server(m_server);
 	if (cmdInfo.m_nShellCommand == CCommandLineInfo::FileOpen && !cmdInfo.m_strFileName.IsEmpty())
 		dlg.open(static_cast<string>(cmdInfo.m_strFileName));
 	dlg.DoModal();
@@ -77,17 +66,3 @@ BOOL CXBTClientApp::InitInstance()
 	return FALSE;
 }
 
-unsigned int CXBTClientApp::backend_thread(void* p)
-{
-	reinterpret_cast<CXBTClientApp*>(p)->m_server.run();
-	return 0;
-}
-
-int CXBTClientApp::ExitInstance() 
-{
-	m_server.stop();
-	if (m_server_thread)
-		WaitForSingleObject(m_server_thread->m_hThread, INFINITE);	
-	delete m_server_thread;
-	return CWinApp::ExitInstance();
-}
