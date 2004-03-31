d0763187d84c6c0dcf7584b6c6db1132182922e4
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-03-31 15:56:51 +0000

    no message

--- xbt/XBT Tracker/XBT Tracker.cpp
+++ xbt/XBT Tracker/XBT Tracker.cpp
@@ -15,30 +15,11 @@ int main(int argc, char* argv[])
 	Cdatabase database;
 	Cxcc_error error;
 	Cstatic_config static_config;
-	Csocket l0, l1;
 	if (error = static_config.read("xbt_tracker.conf"))
 		cerr << error.message() << endl;
-	if (error = database.open(static_config.mysql_host, static_config.mysql_user, static_config.mysql_password, static_config.mysql_db, true))
+	else if (error = database.open(static_config.mysql_host, static_config.mysql_user, static_config.mysql_password, static_config.mysql_db, true))
 		cerr << error.message() << endl;
-	if (l0.open(SOCK_STREAM) == INVALID_SOCKET
-		|| l1.open(SOCK_DGRAM) == INVALID_SOCKET)
-		cerr << "socket failed: " << WSAGetLastError() << endl;
-	else if (l0.bind(htonl(INADDR_ANY), htons(2710))
-		|| l1.bind(htonl(INADDR_ANY), htons(2710)))
-		cerr << "bind failed: " << WSAGetLastError() << endl;
-	else if (l0.listen())
-		cerr << "listen failed: " << WSAGetLastError() << endl;
-	else
-	{
-#ifndef WIN32
-		if (daemon(true, false))
-			cerr << "daemon failed" << endl;
-		ofstream("xbt_tracker.pid") << getpid() << endl;
-#endif
-		Cserver server(database);
-		server.read_config();
-		server.run(l0, l1);
-	}
+	Cserver(database).run();
 #ifdef WIN32
 	WSACleanup();
 #endif
