0f8b270a110d68917cf6c0f345a06f710f96908c
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-04-24 15:50:31 +0000

    no message

--- trunk/xbt/XBT Tracker/xbt_files.php
+++ trunk/xbt/XBT Tracker/xbt_files.php
@@ -84,4 +84,8 @@
 	echo("</table>");
 ?>
 <hr>
-<a href="http://sourceforge.net/projects/xbtt/"><img src="http://sourceforge.net/sflogo.php?group_id=94951;type=1" alt="XBT project at SF"></a>
+<div>
+	<a href="http://sourceforge.net/projects/xbtt/"><img src="http://sourceforge.net/sflogo.php?group_id=94951;type=1" alt="XBT project at SF"></a>
+	<a href="http://w3.org/"><img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"></a>
+	<a href="http://w3.org/"><img src="http://w3.org/Icons/valid-html401" alt="Valid HTML 4.01!"></a>
+</div>
