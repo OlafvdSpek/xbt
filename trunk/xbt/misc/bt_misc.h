c65dc56fddeaa5fc51d20f2169f4c1c725fbc4bc
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-05-06 13:16:52 +0000

    no message

--- trunk/xbt/misc/bt_misc.h
+++ trunk/xbt/misc/bt_misc.h
@@ -56,4 +56,43 @@ enum
 	uta_error,
 };
 
+enum
+{
+	uti_connection_id = 0,
+	uti_action = 8,
+	uti_transaction_id = 12,
+	uti_size = 16,
+
+	utic_size = 16,
+	
+	utia_info_hash = 16,
+	utia_peer_id = 36,
+	utia_downloaded = 56,
+	utia_left = 64,
+	utia_uploaded = 72,
+	utia_event = 80,
+	utia_ipa = 84,
+	utia_key = 88,
+	utia_num_want = 92,
+	utia_port = 96,
+	utia_size = 96,
+
+	utis_size = 16,
+
+	uto_action = 0,
+	uto_transaction_id = 4,
+
+	utoc_connection_id = 8,
+	utoc_size = 16,
+
+	utoa_interval = 8,
+	utoa_leechers = 12,
+	utoa_seeders = 16,
+	utoa_size = 20,
+
+	utos_size = 8,
+
+	utoe_size = 8,
+};
+
 #endif // !defined(AFX_BT_MISC_H__C8A447CF_4F41_429E_9437_55453B6A85D8__INCLUDED_)
