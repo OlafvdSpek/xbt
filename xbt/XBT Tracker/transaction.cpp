17c7a7ea9775d46411146e32c962fe28d125c8a9
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-05-06 13:16:52 +0000

    no message

--- xbt/XBT Tracker/transaction.cpp
+++ xbt/XBT Tracker/transaction.cpp
@@ -10,6 +10,27 @@
 #include "server.h"
 #include "sha1.h"
 
+template <class T>
+static T read(const char* r, const char* r_end)
+{
+	T v = 0;
+	for (int i = 0; i < sizeof(T); i++)
+		v = v << 8 | *reinterpret_cast<const unsigned char*>(r++);
+	return v;
+}
+
+template <class T>
+static char* write(char* w, T v)
+{
+	w += sizeof(T);
+	for (int i = 0; i < sizeof(T); i++)
+	{
+		*--w = v & 0xff;
+		v >>= 8;
+	}
+	return w + sizeof(T);
+}
+
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
@@ -84,124 +105,120 @@ void Ctransaction::recv()
 		cerr << "recv failed: " << WSAGetLastError() << endl;
 		return;
 	}
-	if (r < sizeof(t_udp_tracker_input))
+	if (r < uti_size)
 		return;
-	const t_udp_tracker_input& uti = *reinterpret_cast<t_udp_tracker_input*>(b);
-	switch (uti.action())
+	switch (read<__int32>(b + uti_action, b + r))
 	{
 	case uta_connect:
-		if (r >= sizeof(t_udp_tracker_input_connect))
-			send_connect(*reinterpret_cast<t_udp_tracker_input_connect*>(b), b + sizeof(t_udp_tracker_input_connect), b + r);
+		if (r >= utic_size)
+			send_connect(b, b + r);
 		break;
 	case uta_announce:
-		if (r >= sizeof(t_udp_tracker_input_announce))
-			send_announce(*reinterpret_cast<t_udp_tracker_input_announce*>(b), b + sizeof(t_udp_tracker_input_announce), b + r);
+		if (r >= utia_size)
+			send_announce(b, b + r);
 		break;
 	case uta_scrape:
-		if (r >= sizeof(t_udp_tracker_input_scrape))
-			send_scrape(*reinterpret_cast<t_udp_tracker_input_scrape*>(b), b + sizeof(t_udp_tracker_input_scrape), b + r);
+		if (r >= utis_size)
+			send_scrape(b, b + r);
 		break;
 	}
 }
 
-void Ctransaction::send_connect(const t_udp_tracker_input_connect& uti, const char* r, const char* r_end)
+void Ctransaction::send_connect(const char* r, const char* r_end)
 {
-	if (!m_server.anonymous_connect() && !authenticate(&uti, r_end))
+	if (!m_server.anonymous_connect() && !authenticate(r, r_end))
 		return;
-	t_udp_tracker_output_connect uto;
-	uto.action(uta_connect);
-	uto.transaction_id(uti.transaction_id());
-	uto.m_connection_id = connection_id();
-	send(&uto, sizeof(t_udp_tracker_output_connect));
+	const int cb_d = 2 << 10;
+	char d[cb_d];
+	write<__int32>(d + uto_action, uta_connect);
+	write<__int32>(d + uto_transaction_id, read<__int32>(r + uti_transaction_id, r_end));
+	write<__int64>(d + utoc_connection_id, connection_id());
+	send(d, utoc_size);
 }
 
-void Ctransaction::send_announce(const t_udp_tracker_input_announce& uti, const char* r, const char* r_end)
+void Ctransaction::send_announce(const char* r, const char* r_end)
 {
-	if (uti.m_connection_id != connection_id())
+	if (read<__int64>(r + uti_connection_id, r_end) != connection_id())
 		return;
-	const Cserver::t_user* user = authenticate(&uti, r_end);
+	const Cserver::t_user* user = authenticate(r, r_end);
 	if (!m_server.anonymous_announce() && !user)
 	{
-		send_error(uti, "access denied");
+		send_error(r, r_end, "access denied");
 		return;
 	}
 	Ctracker_input ti;
-	ti.m_downloaded = uti.downloaded();
-	ti.m_event = static_cast<Ctracker_input::t_event>(uti.event());
-	ti.m_info_hash = uti.info_hash();
-	ti.m_ipa = uti.ipa() && is_private_ipa(m_a.sin_addr.s_addr)
-		? uti.ipa()
+	ti.m_downloaded = read<__int64>(r + utia_downloaded, r_end);
+	ti.m_event = static_cast<Ctracker_input::t_event>(read<__int32>(r + utia_event, r_end));
+	ti.m_info_hash.assign(r + utia_info_hash, 20);
+	ti.m_ipa = read<__int32>(r + utia_ipa, r_end) && is_private_ipa(m_a.sin_addr.s_addr)
+		? htonl(read<__int32>(r + utia_ipa, r_end))
 		: m_a.sin_addr.s_addr;
-	ti.m_left = uti.left();
-	ti.m_num_want = uti.num_want();
-	ti.m_peer_id = uti.peer_id();
-	ti.m_port = uti.port();
-	ti.m_uploaded = uti.uploaded();
+	ti.m_left = read<__int64>(r + utia_left, r_end);
+	ti.m_num_want = read<__int32>(r + utia_num_want, r_end);
+	ti.m_peer_id.assign(r + utia_peer_id, 20);
+	ti.m_port = htons(read<__int16>(r + utia_port, r_end));
+	ti.m_uploaded = read<__int64>(r + utia_uploaded, r_end);
 	m_server.insert_peer(ti, ti.m_ipa == m_a.sin_addr.s_addr, true, user ? user->uid : 0);
 	const Cserver::t_file* file = m_server.file(ti.m_info_hash);
 	if (!file)
 	{
-		send_error(uti, bts_unregistered_torrent);
+		send_error(r, r_end, bts_unregistered_torrent);
 		return;
 	}
-	const int cb_b = 2 << 10;
-	char b[cb_b];
-	t_udp_tracker_output_announce& uto = *reinterpret_cast<t_udp_tracker_output_announce*>(b);
-	uto.action(uta_announce);
-	uto.transaction_id(uti.transaction_id());
-	uto.interval(m_server.announce_interval());
-	uto.leechers(file->leechers);
-	uto.seeders(file->seeders);
+	const int cb_d = 2 << 10;
+	char d[cb_d];
+	write<__int32>(d + uto_action, uta_announce);
+	write<__int32>(d + uto_transaction_id, read<__int32>(r + uti_transaction_id, r_end));
+	write<__int32>(d + utoa_interval, m_server.announce_interval());
+	write<__int32>(d + utoa_leechers, file->leechers);
+	write<__int32>(d + utoa_seeders, file->seeders);
 	Cannounce_output_udp o;
-	o.w(b + sizeof(t_udp_tracker_output_announce));
+	o.w(d + utoa_size);
 	file->select_peers(ti, o);
-	send(b, o.w() - b);
+	send(d, o.w() - d);
 }
 
-void Ctransaction::send_scrape(const t_udp_tracker_input_scrape& uti, const char* r, const char* r_end)
+void Ctransaction::send_scrape(const char* r, const char* r_end)
 {
-	if (uti.m_connection_id != connection_id())
+	if (read<__int64>(r + uti_connection_id, r_end) != connection_id())
 		return;
-	if (!m_server.anonymous_scrape() && !authenticate(&uti, r_end))
+	if (!m_server.anonymous_scrape() && !authenticate(r, r_end))
 	{
-		send_error(uti, "access denied");
+		send_error(r, r_end, "access denied");
 		return;
 	}
-	const int cb_b = 2 << 10;
-	char b[cb_b];
-	t_udp_tracker_output_scrape& uto = *reinterpret_cast<t_udp_tracker_output_scrape*>(b);
-	uto.transaction_id(uti.transaction_id());
-	uto.action(uta_scrape);
-	t_udp_tracker_output_file* file = reinterpret_cast<t_udp_tracker_output_file*>(b + sizeof(t_udp_tracker_output_scrape));
-	for (; r + 20 <= r_end && reinterpret_cast<char*>(file + 1) <= b + cb_b; r += 20)
+	const int cb_d = 2 << 10;
+	char d[cb_d];
+	write<__int32>(d + uto_action, uta_scrape);
+	write<__int32>(d + uto_transaction_id, read<__int32>(r + uti_transaction_id, r_end));
+	char* w = d + utos_size;
+	for (; r + 20 <= r_end && w + 12 <= d + cb_d; r += 20)
 	{
-		const Cserver::t_file* i = m_server.file(string(r, 20));
-		if (!i)
+		const Cserver::t_file* file = m_server.file(string(r, 20));
+		if (file)
 		{
-			file->complete(0);
-			file->downloaded(0);
-			file->incomplete(0);
+			w = write<__int32>(w, file->seeders);
+			w = write<__int32>(w, file->completed);
+			w = write<__int32>(w, file->leechers);
 		}
 		else
 		{
-			file->complete(i->seeders);
-			file->downloaded(i->completed);
-			file->incomplete(i->leechers);
+			w = write<__int32>(w, 0);
+			w = write<__int32>(w, 0);
+			w = write<__int32>(w, 0);
 		}
-		file++;
 	}
-	send(b, reinterpret_cast<char*>(file) - b);
+	send(d, w - d);
 }
 
-void Ctransaction::send_error(const t_udp_tracker_input& uti, const string& msg)
+void Ctransaction::send_error(const char* r, const char* r_end, const string& msg)
 {
-	const int cb_b = 2 << 10;
-	char b[cb_b];
-	t_udp_tracker_output_error& uto = *reinterpret_cast<t_udp_tracker_output_error*>(b);
-	uto.action(uta_error);
-	uto.transaction_id(uti.transaction_id());
-	memcpy(b + sizeof(t_udp_tracker_output_error), msg.c_str(), msg.length());
-	send(b, sizeof(t_udp_tracker_output_error) + msg.length());
+	const int cb_d = 2 << 10;
+	char d[cb_d];
+	write<__int32>(d + uto_action, uta_error);
+	write<__int32>(d + uto_transaction_id, read<__int32>(r + uti_transaction_id, r_end));
+	memcpy(d + utoe_size, msg.c_str(), msg.length());
+	send(d, utoe_size + msg.length());
 }
 
 void Ctransaction::send(const void* b, int cb_b)
