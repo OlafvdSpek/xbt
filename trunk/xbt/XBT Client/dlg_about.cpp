3d478420287614a3bbdeb56d9d8863d275605c89
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-07-03 22:30:16 +0000

    no message

--- trunk/xbt/XBT Client/dlg_about.cpp
+++ trunk/xbt/XBT Client/dlg_about.cpp
@@ -27,6 +27,7 @@ void Cdlg_about::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(Cdlg_about)
+	DDX_Control(pDX, IDC_LICENSE, m_license);
 	DDX_Control(pDX, IDC_SITE, m_site);
 	//}}AFX_DATA_MAP
 }
@@ -35,6 +36,7 @@ void Cdlg_about::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(Cdlg_about, CDialog)
 	//{{AFX_MSG_MAP(Cdlg_about)
 	ON_BN_CLICKED(IDC_SITE, OnSite)
+	ON_BN_CLICKED(IDC_LICENSE, OnLicense)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -50,7 +52,13 @@ BOOL Cdlg_about::OnInitDialog()
 {
 	CDialog::OnInitDialog();
 	
+	m_license.ModifyStyle(0, SS_NOTIFY);
 	m_site.ModifyStyle(0, SS_NOTIFY);
 	
 	return true;
+}
+
+void Cdlg_about::OnLicense() 
+{
+	ShellExecute(m_hWnd, "open", "http://gnu.org/copyleft/gpl.html", NULL, NULL, SW_SHOW);	
 }
