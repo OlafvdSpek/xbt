0417746137f404fe6013d062cd08c89c3f794031
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-07-04 22:27:39 +0000

    no message

--- xbt/BT Test/stdafx.h
+++ xbt/BT Test/stdafx.h
@@ -10,16 +10,11 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#pragma comment(lib, "ws2_32.lib")
-#pragma warning(disable: 4786)
-
-#define FD_SETSIZE 1024
-#define atoll _atoi64
+#pragma warning(disable: 4503 4786 4800)
 
 #include <cassert>
 #include <ctime>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
 #include <list>
 #include <map>
@@ -27,13 +22,40 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <windows.h>
 
 using namespace std;
 
+#ifdef WIN32
+#define FD_SETSIZE 1024
+
+#include <windows.h>
+
+#define atoll _atoi64
+#define for if (0) {} else for
+
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <cstdio>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
+
+typedef char __int8;
+typedef short __int16;
+typedef int __int32;
+typedef long long __int64;
+#endif
 #include "bt_misc.h"
-#include "/vc/xf2/misc/virtual_binary.h"
-#include "sha1.h"
+#include "bvalue.h"
+#include "socket.h"
+#include "virtual_binary.h"
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
