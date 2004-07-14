7730c7db1c080720136c40395b2bed45efb4b485
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-07-14 12:56:32 +0000

    no message

--- trunk/xbt/XBT Tracker/xbt_files.php
+++ trunk/xbt/XBT Tracker/xbt_files.php
@@ -1,5 +1,5 @@
 <?php
-	ob_start(ob_gzhandler);
+	ob_start("ob_gzhandler");
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <link rel=stylesheet href="xbt.css">
@@ -10,34 +10,34 @@
 	mysql_select_db("xbt");
 	$results = mysql_query("select sum(announced_http) announced_http, sum(announced_http_compact) announced_http_compact, sum(announced_http_no_peer_id) announced_http_no_peer_id, sum(announced_udp) announced_udp, sum(scraped_http) scraped_http, sum(scraped_udp) scraped_udp, sum(completed) completed, sum(started) started, sum(stopped) stopped, sum(leechers) leechers, sum(seeders) seeders, sum(leechers or seeders) torrents from xbt_files");
 	$result = mysql_fetch_assoc($results);
-	$result[announced] = $result[announced_http] + $result[announced_http_compact] + $result[announced_http_no_peer_id] + $result[announced_udp];
-	$result[scraped] = $result[scraped_http] + $result[scraped_udp];
-	$result[peers] = $result[leechers] + $result[seeders];
+	$result['announced'] = $result['announced_http'] + $result['announced_http_compact'] + $result['announced_http_no_peer_id'] + $result['announced_udp'];
+	$result['scraped'] = $result['scraped_http'] + $result['scraped_udp'];
+	$result['peers'] = $result['leechers'] + $result['seeders'];
 	echo("<table>");
-	printf("<tr><th align=right>announced<td align=right>%d<td align=right>100 %%", $result[announced]);
-	if ($result[announced])
+	printf("<tr><th align=right>announced<td align=right>%d<td align=right>100 %%", $result['announced']);
+	if ($result['announced'])
 	{
-		printf("<tr><th align=right>announced_http<td align=right>%d<td align=right>%d %%", $result[announced_http], $result[announced_http] * 100 / $result[announced]);
-		printf("<tr><th align=right>announced_http_compact<td align=right>%d<td align=right>%d %%", $result[announced_http_compact], $result[announced_http_compact] * 100 / $result[announced]);
-		printf("<tr><th align=right>announced_http_no_peer_id<td align=right>%d<td align=right>%d %%", $result[announced_http_no_peer_id], $result[announced_http_no_peer_id] * 100 / $result[announced]);
-		printf("<tr><th align=right>announced_udp<td align=right>%d<td align=right>%d %%", $result[announced_udp], $result[announced_udp] * 100 / $result[announced]);
+		printf("<tr><th align=right>announced_http<td align=right>%d<td align=right>%d %%", $result['announced_http'], $result['announced_http'] * 100 / $result['announced']);
+		printf("<tr><th align=right>announced_http_compact<td align=right>%d<td align=right>%d %%", $result['announced_http_compact'], $result['announced_http_compact'] * 100 / $result['announced']);
+		printf("<tr><th align=right>announced_http_no_peer_id<td align=right>%d<td align=right>%d %%", $result['announced_http_no_peer_id'], $result['announced_http_no_peer_id'] * 100 / $result['announced']);
+		printf("<tr><th align=right>announced_udp<td align=right>%d<td align=right>%d %%", $result['announced_udp'], $result['announced_udp'] * 100 / $result['announced']);
 	}
-	printf("<tr><th align=right>scraped<td align=right>%d<td align=right>100 %%", $result[scraped]);
-	if ($result[scraped])
+	printf("<tr><th align=right>scraped<td align=right>%d<td align=right>100 %%", $result['scraped']);
+	if ($result['scraped'])
 	{
-		printf("<tr><th align=right>scraped_http<td align=right>%d<td align=right>%d %%", $result[scraped_http], $result[scraped_http] * 100 / $result[scraped]);
-		printf("<tr><th align=right>scraped_udp<td align=right>%d<td align=right>%d %%", $result[scraped_udp], $result[scraped_udp] * 100 / $result[scraped]);
+		printf("<tr><th align=right>scraped_http<td align=right>%d<td align=right>%d %%", $result['scraped_http'], $result['scraped_http'] * 100 / $result['scraped']);
+		printf("<tr><th align=right>scraped_udp<td align=right>%d<td align=right>%d %%", $result['scraped_udp'], $result['scraped_udp'] * 100 / $result['scraped']);
 	}
-	printf("<tr><th align=right>completed<td align=right>%d<td>", $result[completed]);
-	printf("<tr><th align=right>started<td align=right>%d<td>", $result[started]);
-	printf("<tr><th align=right>stopped<td align=right>%d<td>", $result[stopped]);
-	printf("<tr><th align=right>peers<td align=right>%d<td align=right>100 %%", $result[peers]);
-	if ($result[peers])
+	printf("<tr><th align=right>completed<td align=right>%d<td>", $result['completed']);
+	printf("<tr><th align=right>started<td align=right>%d<td>", $result['started']);
+	printf("<tr><th align=right>stopped<td align=right>%d<td>", $result['stopped']);
+	printf("<tr><th align=right>peers<td align=right>%d<td align=right>100 %%", $result['peers']);
+	if ($result['peers'])
 	{
-		printf("<tr><th align=right>leechers<td align=right>%d<td align=right>%d %%", $result[leechers], $result[leechers] * 100 / $result[peers]);
-		printf("<tr><th align=right>seeders<td align=right>%d<td align=right>%d %%", $result[seeders], $result[seeders] * 100 / $result[peers]);
+		printf("<tr><th align=right>leechers<td align=right>%d<td align=right>%d %%", $result['leechers'], $result['leechers'] * 100 / $result['peers']);
+		printf("<tr><th align=right>seeders<td align=right>%d<td align=right>%d %%", $result['seeders'], $result['seeders'] * 100 / $result['peers']);
 	}
-	printf("<tr><th align=right>torrents<td align=right>%d<td>", $result[torrents]);
+	printf("<tr><th align=right>torrents<td align=right>%d<td>", $result['torrents']);
 	printf("<tr><th align=right>time<td align=right colspan=2>%s", gmdate("Y-m-d H:i:s"));
 	echo("</table>");
 	echo("<hr>");
@@ -58,43 +58,43 @@
 	while ($result = mysql_fetch_assoc($results))
 	{
 		echo("<tr>");
-		printf("<td align=right>%d", $result[fid]);
-		printf("<td>%s", bin2hex($result[info_hash]));
+		printf("<td align=right>%d", $result['fid']);
+		printf("<td>%s", bin2hex($result['info_hash']));
 		echo("<td align=right>");
-		if ($result[leechers])
-			printf("%d", $result[leechers]);
+		if ($result['leechers'])
+			printf("%d", $result['leechers']);
 		echo("<td align=right>");
-		if ($result[seeders])
-			printf("%d", $result[seeders]);
+		if ($result['seeders'])
+			printf("%d", $result['seeders']);
 		echo("<td align=right>");
-		if ($result[announced_http])
-			printf("%d", $result[announced_http]);
+		if ($result['announced_http'])
+			printf("%d", $result['announced_http']);
 		echo("<td align=right>");
-		if ($result[announced_http_compact])
-			printf("%d", $result[announced_http_compact]);
+		if ($result['announced_http_compact'])
+			printf("%d", $result['announced_http_compact']);
 		echo("<td align=right>");
-		if ($result[announced_http_no_peer_id])
-			printf("%d", $result[announced_http_no_peer_id]);
+		if ($result['announced_http_no_peer_id'])
+			printf("%d", $result['announced_http_no_peer_id']);
 		echo("<td align=right>");
-		if ($result[announced_udp])
-			printf("%d", $result[announced_udp]);
+		if ($result['announced_udp'])
+			printf("%d", $result['announced_udp']);
 		echo("<td align=right>");
-		if ($result[scraped_http])
-			printf("%d", $result[scraped_http]);
+		if ($result['scraped_http'])
+			printf("%d", $result['scraped_http']);
 		echo("<td align=right>");
-		if ($result[scraped_udp])
-			printf("%d", $result[scraped_udp]);
+		if ($result['scraped_udp'])
+			printf("%d", $result['scraped_udp']);
 		echo("<td align=right>");
-		if ($result[completed])
-			printf("%d", $result[completed]);
+		if ($result['completed'])
+			printf("%d", $result['completed']);
 		echo("<td align=right>");
-		if ($result[started])
-			printf("%d", $result[started]);
+		if ($result['started'])
+			printf("%d", $result['started']);
 		echo("<td align=right>");
-		if ($result[stopped])
-			printf("%d", $result[stopped]);
-		printf("<td>%s", gmdate("Y-m-d H:i:s", $result[mtime]));
-		printf("<td>%s", gmdate("Y-m-d H:i:s", $result[ctime]));
+		if ($result['stopped'])
+			printf("%d", $result['stopped']);
+		printf("<td>%s", gmdate("Y-m-d H:i:s", $result['mtime']));
+		printf("<td>%s", gmdate("Y-m-d H:i:s", $result['ctime']));
 	}
 	echo("</table>");
 ?>
