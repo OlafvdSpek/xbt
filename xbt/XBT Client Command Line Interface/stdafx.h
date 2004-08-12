adc2dcf67a0891268c99cf23218dec9d4c2572aa
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-08-12 16:59:41 +0000

    no message

---
+++ xbt/XBT Client Command Line Interface/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__65F76B17_AF08_4DB2_B7D8_399776542FE6__INCLUDED_)
+#define AFX_STDAFX_H__65F76B17_AF08_4DB2_B7D8_399776542FE6__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#pragma warning(disable: 4554 4786 4800)
+
+#define atoll _atoi64
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include "socket.h"
+
+using namespace std;
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__65F76B17_AF08_4DB2_B7D8_399776542FE6__INCLUDED_)
