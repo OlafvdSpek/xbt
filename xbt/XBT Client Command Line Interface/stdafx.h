6d9cb230e848b3dcf589c4fbea49fe3d4bc665c4
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-08-28 19:13:05 +0000

    no message

--- xbt/XBT Client Command Line Interface/stdafx.h
+++ xbt/XBT Client Command Line Interface/stdafx.h
@@ -10,16 +10,67 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#pragma warning(disable: 4554 4786 4800)
+#pragma warning(disable: 4503 4554 4786 4800)
 
-#define atoll _atoi64
-
+#include <cassert>
+#include <ctime>
+#include <fstream>
 #include <iostream>
+#include <list>
 #include <map>
+#include <set>
+#include <sstream>
+#include <string>
 #include <vector>
-#include "socket.h"
 
 using namespace std;
+
+#ifdef WIN32
+#define FD_SETSIZE 1024
+
+#include <io.h>
+#include <windows.h>
+
+#define atoll _atoi64
+#define for if (0) {} else for
+
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <cstdio>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define O_BINARY 0
+#define _lseeki64 lseek
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
+
+typedef char __int8;
+typedef short __int16;
+typedef int __int32;
+typedef long long __int64;
+
+inline __int64 max(__int64 a, __int64 b)
+{
+	return a > b ? a : b;
+}
+
+inline __int64 min(__int64 a, __int64 b)
+{
+	return a < b ? a : b;
+}
+#endif
+#include "bt_misc.h"
+#include "bvalue.h"
+#include "socket.h"
+#include "virtual_binary.h"
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
