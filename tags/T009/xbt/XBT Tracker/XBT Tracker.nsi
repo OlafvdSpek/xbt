9088e157fe93d1d612a413d1c75c4088bb97562b
nobody <nobody@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-09-06 20:35:53 +0000

    This commit was manufactured by cvs2svn to create tag 'T009'.

---
+++ tags/T009/xbt/XBT Tracker/XBT Tracker.nsi
@@ -0,0 +1,34 @@
+!define UPGRADEDLL_NOREGISTER
+!define VERSION "0.0.9"
+!include "UpgradeDLL.nsh"
+
+Name "XBT Tracker ${VERSION}"
+Outfile "XBT_Tracker-${VERSION}.exe"
+InstallDir "$PROGRAMFILES\XBT\Tracker"
+Page directory
+Page instfiles
+Section "Install"
+	SetShellVarContext all
+	SetOutPath "$INSTDIR"
+	!insertmacro UpgradeDLL "zlib1.dll" "$SYSDIR\zlib1.dll" "$SYSDIR"
+
+	File release\*.exe
+	File xbt.css
+	File xbt_files.php
+	File xbt_tracker.conf.default
+	File xbt_tracker.sql
+	SetOverwrite off
+	File /oname=xbt_tracker.conf xbt_tracker.conf.default
+	WriteUninstaller "$INSTDIR\Uninstall.exe"
+	CreateShortCut "$SMPROGRAMS\XBT Tracker.lnk" "$INSTDIR\XBT Tracker.exe"
+	WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\XBT Tracker" "DisplayName" "XBT Tracker ${VERSION}"
+	WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\XBT Tracker" "UninstallString" '"$INSTDIR\Uninstall.exe"'
+	WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\XBT Tracker" "NoModify" 1
+SectionEnd
+
+Section "Uninstall"
+	SetShellVarContext all
+	Delete "$SMPROGRAMS\XBT Tracker.lnk"
+	RMDir /r "$INSTDIR"
+	RMDir "$PROGRAMFILES\XBT"
+SectionEnd
---
+++ tags/T009/xbt/misc/sql/sql_result.h
@@ -0,0 +1,118 @@
+// sql_result.h: interface for the Csql_result class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_SQL_RESULT_H__EA1254C8_2222_11D5_B606_0000B4936994__INCLUDED_)
+#define AFX_SQL_RESULT_H__EA1254C8_2222_11D5_B606_0000B4936994__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#ifdef _MSC_VER
+#include <windows.h>
+#include <mysql.h>
+#else
+#include <mysql/mysql.h>
+#endif
+#include "virtual_binary.h"
+
+class Csql_result_source
+{
+public:
+	Csql_result_source(MYSQL_RES* h)
+	{
+		m_h = h;
+		mc_references = 1;
+	}
+
+	Csql_result_source* attach()
+	{
+		this && mc_references++;
+		return this;
+	}
+
+	void detach()
+	{
+		if (this && !--mc_references)
+			mysql_free_result(m_h);
+	}
+
+	MYSQL_RES* h() const
+	{
+		return m_h;
+	}
+private:
+	MYSQL_RES* m_h;
+	int mc_references;
+};
+
+class Csql_row  
+{
+public:
+	const Csql_row& operator=(const Csql_row& v);
+	Csql_row();
+	Csql_row(MYSQL_ROW data, unsigned long* sizes, Csql_result_source* source);
+	Csql_row(const Csql_row& v);
+	~Csql_row();
+
+	operator bool() const
+	{
+		return m_data;
+	}
+
+	const char* f(int i) const
+	{
+		return m_data[i];
+	}
+
+	int size(int i) const
+	{
+		return m_sizes[i];
+	}
+
+	const char* f(int i, const char* d) const
+	{
+		return f(i) ? f(i) : d;
+	}
+
+	int f_int(int i) const
+	{
+		return atoi(f(i));
+	}
+
+	int f_int(int i, int d) const
+	{
+		return f(i) ? atoi(f(i)) : d;
+	}
+
+	Cvirtual_binary f_vdata(int i) const
+	{
+		return Cvirtual_binary(f(i), size(i));
+	}
+private:
+	MYSQL_ROW m_data;
+	unsigned long* m_sizes;
+	Csql_result_source* m_source;
+};
+
+class Csql_result  
+{
+public:
+	int c_fields() const;
+	int c_rows() const;
+	Csql_row fetch_row() const;
+	const Csql_result& operator=(const Csql_result& v);
+	Csql_result(MYSQL_RES* h);
+	Csql_result(const Csql_result& v);
+	~Csql_result();
+private:
+	MYSQL_RES* h() const
+	{
+		return m_source->h();
+	}
+
+	Csql_result_source* m_source;
+};
+
+#endif // !defined(AFX_SQL_RESULT_H__EA1254C8_2222_11D5_B606_0000B4936994__INCLUDED_)
