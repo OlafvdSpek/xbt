10e27e8e81533faabd75f2bdf256f29bf389b005
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-09-23 11:47:02 +0000

    no message

--- trunk/xbt/XBT IPB2 Mod/xbt_ipb_mod.sql
+++ trunk/xbt/XBT IPB2 Mod/xbt_ipb_mod.sql
@@ -3,3 +3,4 @@ alter table ibf_posts add bt_name varchar(255);
 alter table ibf_posts add bt_size bigint;
 alter table ibf_posts add bt_tracker varchar(255);
 alter table ibf_topics add bt_info_hash blob;
+insert into ibf_attachments_type (atype_extension, atype_mimetype, atype_img) values ('torrent', 'application/x-bittorrent', 'folder_mime_types/zip.gif');
