5f07e9f47dba6ee0d8f57b703a3be79951bfb088
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-10-12 23:07:44 +0000

    no message

--- trunk/xbt/BT Test/server.cpp
+++ trunk/xbt/BT Test/server.cpp
@@ -86,7 +86,7 @@ Cserver::~Cserver()
 static string new_peer_id()
 {
 	string v;
-	v = "XBT027--";
+	v = "XBT028--";
 	v.resize(20);
 	for (size_t i = 8; i < v.size(); i++)
 		v[i] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwyxz"[rand() % 62];
