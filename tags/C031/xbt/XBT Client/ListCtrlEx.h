e59fe95a4d9e5ffb731ce25cb8aa1f60df6c8564
nobody <nobody@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-10-25 19:51:45 +0000

    This commit was manufactured by cvs2svn to create tag 'C031'.

---
+++ tags/C031/xbt/XBT Client/ListCtrlEx.h
@@ -0,0 +1,33 @@
+#if !defined(AFX_LISTCTRLEX_H__1A7B4E56_9CB8_4BA4_B5AC_AF5DA38C4285__INCLUDED_)
+#define AFX_LISTCTRLEX_H__1A7B4E56_9CB8_4BA4_B5AC_AF5DA38C4285__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CListCtrlEx: public CListCtrl
+{
+public:
+	//{{AFX_VIRTUAL(CListCtrlEx)
+	public:
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	protected:
+	virtual void PreSubclassWindow();
+	//}}AFX_VIRTUAL
+public:
+	DWORD GetItemData(int nItem) const;
+	void auto_size();
+protected:
+	//{{AFX_MSG(CListCtrlEx)
+	afx_msg void OnCustomDraw(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_LISTCTRLEX_H__1A7B4E56_9CB8_4BA4_B5AC_AF5DA38C4285__INCLUDED_)
