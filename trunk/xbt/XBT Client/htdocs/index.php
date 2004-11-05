19ac0c8891d11f41c36428b05078aa62cf6dc6f1
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-11-05 12:03:37 +0000

    no message

---
+++ trunk/xbt/XBT Client/htdocs/index.php
@@ -0,0 +1,58 @@
+<?php
+	require_once('common.php');
+	require_once('templates.php');
+
+	set_time_limit(5);
+	$s = fsockopen($config['client_host'], $config['client_port']);
+	if ($s === false)
+		die('fsockopen failed');
+	if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name']))
+	{
+		$d = file_get_contents($_FILES['file']['tmp_name']);
+		send_string($s, sprintf('d6:action12:open torrent7:torrent%se', $d));
+		recv_string($s);
+	}
+	$actions = array
+	(
+		'close' => 'close torrent',
+		'pause' => 'pause torrent',
+		'priority_high' => 'set priority',
+		'priority_normal' => 'set priority',
+		'priority_low' => 'set priority',
+		'unpause' => 'unpause torrent',
+	);
+	switch ($_REQUEST['a'])
+	{
+	case 'open':
+		break;
+	default:
+		if (array_key_exists($_REQUEST['a'], $actions))
+		{
+			$action = $actions[$_REQUEST['a']];
+			foreach ($_REQUEST as $name => $value)
+			{
+				$name = urldecode($name);
+				if (strlen($name) != 20 || $value != 'on')
+					continue;
+				send_string($s, sprintf('d6:action%d:%s4:hash20:%se', strlen($action), $action, $name));
+				recv_string($s);
+			}
+		}
+	}
+	if ($_SERVER['REQUEST_METHOD'] != 'GET')
+	{
+		header('location: ' . $_SERVER['SCRIPT_NAME']);
+		exit();
+	}
+	send_string($s, 'd6:action10:get statuse');
+	$v = recv_string($s);
+	$v = bdec($v);
+	$rows = '';
+	foreach ($v['value']['files']['value'] as $info_hash => $file)
+	{
+		$rows .= template_torrent(array_merge($file['value'], array('info_hash' => array('value' => $info_hash))));
+	}
+	echo(template_top());
+	echo(template_torrents(array('rows' => $rows)));
+	echo(template_bottom());
+?>
---
+++ trunk/xbt/XBT Client/htdocs/templates.php
@@ -0,0 +1,136 @@
+<?php
+	function b2a($v)
+	{
+		for ($l = 0; $v < -9999 || $v > 9999; $l++)
+			$v /= 1024;
+		$a = array('', 'k', 'm', 'g', 't', 'p');
+		return sprintf('%d %s', $v, $a[$l]);
+	}
+
+	function priority2a($v)
+	{
+		switch ($v)
+		{
+		case 1:
+			return 'H';
+		case 0:
+			return '';
+		case -1:
+			return 'L';
+		case -10:
+			return 'E';
+		}
+		return $v;
+	}
+
+	function state2a($v)
+	{
+		switch ($v)
+		{
+		case '0':
+			return '';
+		case 1:
+			return 'R';
+		}
+		return $v;
+	}
+
+	function strip_name($v)
+	{
+		$i = strrpos($v, '/');
+		if ($i !== false)
+		$v = substr($v, $i + 1);
+		$i = strrpos($v, '\\');
+		return $i === false ? $v : substr($v, $i + 1);
+
+	}
+
+	function template_top()
+	{
+		$d = '';
+		$d .= '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">';
+		$d .= '<link rel=stylesheet href="xbt.css">';
+		$d .= '<meta http-equiv=refresh content=60>';
+		$d .= '<title>XBT Client</title>';
+		return $d;
+	}
+
+	function template_bottom()
+	{
+		$d = '';
+		$d .= '<hr>';
+		$d .= '<center>';
+		$d .= '<a href="http://sourceforge.net/projects/xbtt/"><img src="http://sourceforge.net/sflogo.php?group_id=94951;type=1" alt="XBT project at SF"></a>';
+		$d .= '</center>';
+		return $d;
+	};
+
+	function template_torrent($v)
+	{
+		$d = "\n";
+		$d .= '<tr>';
+		$d .= sprintf('<td><input type=checkbox name="%s">', urlencode($v['info_hash']['value']));
+		$d .= sprintf('<td align=left>%s', htmlspecialchars(strip_name($v['name']['value'])));
+		$d .= sprintf('<td align=right>%d', ($v['size']['value'] - $v['left']['value']) * 100 / $v['size']['value']);
+		$d .= sprintf('<td align=right>%s', b2a($v['left']['value']));
+		$d .= sprintf('<td align=right>%s', b2a($v['size']['value']));
+		$d .= sprintf('<td align=right>%s', b2a($v['total downloaded']['value']));
+		$d .= sprintf('<td align=right>%s', b2a($v['total uploaded']['value']));
+		$d .= sprintf('<td align=right>%s', b2a($v['down rate']['value']));
+		$d .= sprintf('<td align=right>%s', b2a($v['up rate']['value']));
+		$d .= sprintf('<td align=right>%d', $v['incomplete']['value']);
+		$d .= sprintf('<td align=right>%d', $v['complete']['value']);
+		$d .= sprintf('<td align=right>%s', priority2a($v['priority']['value']));
+		$d .= sprintf('<td align=left>%s', state2a($v['state']['value']));
+		return $d;
+	}
+
+	function template_torrents($v)
+	{
+		$d = '';
+		$d .= '<center>';
+		$d .= '<form action="?" method=post>';
+		$d .= '<table>';
+		$d .= '<caption>Torrents</caption>';
+		$d .= '<th>';
+		$d .= '<th align=left>Name';
+		$d .= '<th align=right>%';
+		$d .= '<th align=right>Left';
+		$d .= '<th align=right>Size';
+		$d .= '<th align=right>Downloaded';
+		$d .= '<th align=right>Uploaded';
+		$d .= '<th align=right>Down rate';
+		$d .= '<th align=right>Up rate';
+		$d .= '<th align=right>Leechers';
+		$d .= '<th align=right>Seeders';
+		$d .= '<th align=right>Priority';
+		$d .= '<th align=right>State';
+		$d .= $v['rows'];
+		$d .= '</table>';
+		$d .= '<br>';
+		$d .= '<select name="a" onchange="this.form.submit();">';
+		$d .= '<option>Do...</option>';
+		$d .= '<option value="start">Start</option>';
+		$d .= '<option value="pause">Pause</option>';
+		$d .= '<option value="unpause">Unpause</option>';
+		$d .= '<option value="stop">Stop</option>';
+		$d .= '<option value="close">Close</option>';
+		$d .= '<option value="set_priority_high">Priority - High</option>';
+		$d .= '<option value="set_priority_normal">Priority - Normal</option>';
+		$d .= '<option value="set_priority_low">Priority - Low</option>';
+		$d .= '</select>';
+		$d .= '</form>';
+		$d .= '</center>';
+		$d .= '<hr>';
+		$d .= '<center>';
+		$d .= '<form action="?" enctype="multipart/form-data" method=post>';
+		$d .= '<table>';
+		$d .= '<tr>';
+		$d .= '<td><input type=file name=file>';
+		$d .= '<td><input type=submit value="Open">';
+		$d .= '</table>';
+		$d .= '</form>';
+		$d .= '</center>';
+		return $d;
+	}
+?>
