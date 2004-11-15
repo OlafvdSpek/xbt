b9510d7198c15af10f2be9056e6d87b47ed222e0
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-11-15 12:24:03 +0000

    no message

--- xbt/misc/alerts.cpp
+++ xbt/misc/alerts.cpp
@@ -17,15 +17,6 @@ static char THIS_FILE[]=__FILE__;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-Calert::Calert(t_level level, const sockaddr_in& source, const string& message)
-{
-	m_time = ::time(NULL);
-	m_level = level;
-	m_message = message;
-	m_source = inet_ntoa(source.sin_addr);
-	m_source += ':' + n(ntohs(source.sin_port));
-}
-
 int Calert::pre_dump() const
 {
 	return m_message.size() + m_source.size() + 16;
