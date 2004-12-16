89a8b23f519db091d3e224e5d2271a39345586a0
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2004-12-16 14:44:45 +0000

    no message

--- trunk/xbt/BT Test/make.sh
+++ trunk/xbt/BT Test/make.sh
@@ -1,2 +1,2 @@
 clear
-g++ -DNDEBUG -I ../misc -I . -O3 -lz -o xbt_client_back_end *.cpp ../misc/*.cpp && strip xbt_client_back_end
+g++ -DNDEBUG -I ../misc -I . -O3 -lz -o xbt_client_backend *.cpp ../misc/*.cpp && strip xbt_client_backend
