e59bb152db08faad72ae09c6f316926bfa61c4b6
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-02-02 14:38:17 +0000

    no message

--- xbt/XBT Tracker/make.bsd.sh
+++ xbt/XBT Tracker/make.bsd.sh
@@ -1,2 +1,2 @@
 clear
-g++ -DNDEBUG -I /usr/local/include -I ../misc -I . -L /usr/local/lib/mysql -O3 -lmysqlclient -o xbt_tracker *.cpp ../misc/*.cpp ../misc/sql/*.cpp && strip xbt_tracker
+g++ -DBSD -DNDEBUG -I /usr/local/include -I ../misc -I . -L /usr/local/lib/mysql -O3 -lmysqlclient -o xbt_tracker *.cpp ../misc/*.cpp ../misc/sql/*.cpp && strip xbt_tracker
