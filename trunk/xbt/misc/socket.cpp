465625f894077ebd19995c27f5bcd6f0c9450568
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-03-09 20:02:33 +0000

    no message

--- trunk/xbt/misc/socket.cpp
+++ trunk/xbt/misc/socket.cpp
@@ -152,6 +152,17 @@ int Csocket::sendto(const void* s, int cb_s, const sockaddr* a, socklen_t cb_a) 
 	return ::sendto(*this, reinterpret_cast<const char*>(s), cb_s, MSG_NOSIGNAL, a, cb_a);
 }
 
+int Csocket::getsockopt(int level, int name, void* v, int& cb_v)
+{
+	return ::getsockopt(*this, level, name, reinterpret_cast<char*>(v), &cb_v);
+}
+
+int Csocket::getsockopt(int level, int name, int& v)
+{
+	int cb_v = sizeof(int);
+	return getsockopt(level, name, &v, cb_v);
+}
+
 int Csocket::setsockopt(int level, int name, const void* v, int cb_v)
 {
 	return ::setsockopt(*this, level, name, reinterpret_cast<const char*>(v), cb_v);
