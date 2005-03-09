a6a84c408626862e97f456e431a7dd63b90f7efc
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-03-09 21:44:02 +0000

    no message

--- trunk/xbt/BT Test/http_link.cpp
+++ trunk/xbt/BT Test/http_link.cpp
@@ -14,23 +14,44 @@
 Chttp_link::Chttp_link(Cserver* server)
 {
 	m_server = server;
+	m_state = 0;
 }
 
 int Chttp_link::pre_select(fd_set* fd_read_set, fd_set* fd_write_set, fd_set* fd_except_set)
 {
-	if (!m_read_b.cb_w())
-		return 0;
-	FD_SET(m_s, fd_read_set);
-	return m_s;
+	switch (m_state)
+	{
+	case 1:
+		FD_SET(m_s, fd_except_set);
+	case 2:
+		FD_SET(m_s, fd_read_set);
+		if (m_write_b.cb_r())
+			FD_SET(m_s, fd_write_set);
+		return m_s;
+	}	
+	return 0;
 }
 
 int Chttp_link::post_select(fd_set* fd_read_set, fd_set* fd_write_set, fd_set* fd_except_set)
 {
-	return m_read_b.cb_w() && FD_ISSET(m_s, fd_read_set) && recv();
+	if (m_s == INVALID_SOCKET)
+		return 1;
+	if (FD_ISSET(m_s, fd_except_set))
+	{
+		int e = 0;
+		m_s.getsockopt(SOL_SOCKET, SO_ERROR, e);
+		alert(Calert::error, "Link: connect failed: " + Csocket::error2a(e));
+		return 1;
+	}
+	return FD_ISSET(m_s, fd_read_set) && recv()
+		|| m_write_b.cb_r() && FD_ISSET(m_s, fd_write_set) && send();
 }
 
 int Chttp_link::recv()
 {
+	m_state = 2;
+	if (!m_read_b.size())
+		m_read_b.size(65 << 10);
 	for (int r; r = m_s.recv(m_read_b.w(), m_read_b.cb_w()); )
 	{
 		if (r == SOCKET_ERROR)
@@ -43,9 +64,57 @@ int Chttp_link::recv()
 		}
 		m_read_b.cb_w(r);
 	}
+	if (m_response_handler)
+		m_response_handler->handle(string(m_read_b.r(), m_read_b.cb_r()));
 	return 1;
 }
 
-void Chttp_link::alert(Calert::t_level, const string&)
+int Chttp_link::send()
 {
+	m_state = 2;
+	for (int r; r = m_s.send(m_write_b.r(), m_write_b.cb_r()); )
+	{
+		if (r == SOCKET_ERROR)
+		{
+			int e = WSAGetLastError();
+			if (e == WSAEWOULDBLOCK)
+				return 0;
+			alert(Calert::debug, "Link: send failed: " + Csocket::error2a(e));
+			return 1;
+		}
+		m_write_b.cb_r(r);
+	}
+	return 0;
+}
+
+void Chttp_link::alert(Calert::t_level level, const string& message)
+{
+	if (m_response_handler)
+		m_response_handler->alert(Calert(level, message));
+
+}
+
+int Chttp_link::set_request(int h, int p, const string& v, Chttp_response_handler* response_handler)
+{
+	assert(m_state == 0);
+	if (m_s.open(SOCK_STREAM) == INVALID_SOCKET)
+		return 1;
+	if (m_s.connect(h, p) && WSAGetLastError() != WSAEINPROGRESS && WSAGetLastError() != WSAEWOULDBLOCK)
+		return 1;
+	m_state = 1;
+	m_response_handler = response_handler;
+	m_write_b.size(v.size() + 1);
+	m_write_b.write(v.c_str(), v.size());
+	return 0;
+}
+
+void Chttp_link::cancel()
+{
+	close();
+}
+
+void Chttp_link::close()
+{
+	m_s.close();
+	m_state = 0;
 }
