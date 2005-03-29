684add0d2a6a1b983b354f8869f573b9242ab0ca
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-03-29 14:52:38 +0000

    no message

--- trunk/xbt/BT Test/server.cpp
+++ trunk/xbt/BT Test/server.cpp
@@ -1128,7 +1128,7 @@ void Cserver::term()
 
 int Cserver::version()
 {
-	return 48;
+	return 49;
 }
 
 Chttp_link* Cserver::http_request(int h, int p, const string& request, Chttp_response_handler* response_handler)
