0c1d47260afe060842d44be3af6dd46b63d6eda8
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-03-29 14:22:52 +0000

    no message

---
+++ xbt/XBT Client/ListCtrlEx1.h
@@ -0,0 +1,54 @@
+#if !defined(AFX_LISTCTRLEX1_H__EB7D38BC_3076_4F00_B25A_2442DA63FAFB__INCLUDED_)
+#define AFX_LISTCTRLEX1_H__EB7D38BC_3076_4F00_B25A_2442DA63FAFB__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "ListCtrlEx.h"
+
+class CListCtrlEx1: public CListCtrlEx
+{
+public:
+	//{{AFX_VIRTUAL(CListCtrlEx1)
+	protected:
+	virtual void PreSubclassWindow();
+	//}}AFX_VIRTUAL
+public:
+	void DeleteAllColumns();
+	int GetColumnID(int index) const;
+	void InsertColumn(int id, const string& name, int format = LVCFMT_LEFT);
+	void ShowColumn(int id, bool show);
+protected:
+	virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
+	//{{AFX_MSG(CListCtrlEx1)
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+private:
+	void DeleteColumn();
+
+	class Ccolumn
+	{
+	public:
+		Ccolumn();
+
+		int format;
+		int index;
+		string name;
+		int order;
+
+		bool show() const
+		{
+			return index != -1;
+		}
+	};
+
+	typedef std::map<int, Ccolumn> Ccolumns;
+
+	Ccolumns m_columns;
+};
+
+//{{AFX_INSERT_LOCATION}}
+
+#endif // !defined(AFX_LISTCTRLEX1_H__EB7D38BC_3076_4F00_B25A_2442DA63FAFB__INCLUDED_)
