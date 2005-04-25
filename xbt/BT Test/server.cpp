8e904e795bc0e66bd0ee89e6dac13b475075eb5f
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-04-25 17:31:41 +0000

    no message

--- xbt/BT Test/server.cpp
+++ xbt/BT Test/server.cpp
@@ -1133,7 +1133,7 @@ void Cserver::term()
 
 int Cserver::version()
 {
-	return 51;
+	return 52;
 }
 
 Chttp_link* Cserver::http_request(int h, int p, const string& request, Chttp_response_handler* response_handler)
--- xbt/XBT Client/XBT Client.nsi
+++ xbt/XBT Client/XBT Client.nsi
@@ -1,5 +1,5 @@
 !define UPGRADEDLL_NOREGISTER
-!define VERSION "0.5.1"
+!define VERSION "0.5.2"
 !include "UpgradeDLL.nsh"
 
 Name "XBT Client ${VERSION}"
