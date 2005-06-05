71668c2a4c670201895215e9bd44294756ec6531
nobody <nobody@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-06-05 07:13:07 +0000

    This commit was manufactured by cvs2svn to create tag 'C056'.

---
+++ tags/C056/xbt/BT Test/version_check_handler.h
@@ -0,0 +1,27 @@
+// version_check_handler.h: interface for the Cversion_check_handler class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_VERSION_CHECK_HANDLER_H__4B9397C3_6058_46EA_B1EA_AC260B78CC18__INCLUDED_)
+#define AFX_VERSION_CHECK_HANDLER_H__4B9397C3_6058_46EA_B1EA_AC260B78CC18__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "http_response_handler.h"
+
+class Cserver;
+
+class Cversion_check_handler: public Chttp_response_handler
+{
+public:
+	virtual void alert(const Calert&);
+	virtual void handle(const string& response);
+	Cversion_check_handler(Cserver&);
+private:
+	Cserver& m_server;
+	int m_version;
+};
+
+#endif // !defined(AFX_VERSION_CHECK_HANDLER_H__4B9397C3_6058_46EA_B1EA_AC260B78CC18__INCLUDED_)
