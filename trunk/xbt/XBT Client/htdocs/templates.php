ef1db38acb83331a9fe106225c426ca265192add
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-06-06 08:54:54 +0000

    no message

--- trunk/xbt/XBT Client/htdocs/templates.php
+++ trunk/xbt/XBT Client/htdocs/templates.php
@@ -104,12 +104,12 @@
 		$d .= sprintf('<td align=right>%s', b2a($v['total uploaded']['value']));
 		$d .= sprintf('<td align=right>%s', b2a($v['down rate']['value']));
 		$d .= sprintf('<td align=right>%s', b2a($v['up rate']['value']));
-		$d .= sprintf('<td align=right>%s', nz($v['incomplete']['value']));
-		if ($v['incomplete total']['value'])
-			$d .= ' / ' . $v['incomplete total']['value'];
-		$d .= sprintf('<td align=right>%s', nz($v['complete']['value']));
-		if ($v['complete total']['value'])
-			$d .= ' / ' . $v['complete total']['value'];
+		$d .= sprintf('<td align=right>');
+		if ($v['incomplete']['value'] || $v['incomplete total']['value'])
+			$d .= $v['incomplete']['value'] . ' / ' . $v['incomplete total']['value'];
+		$d .= sprintf('<td align=right>');
+		if ($v['complete']['value'] || $v['complete total']['value'])
+			$d .= $v['complete']['value'] . ' / ' . $v['complete total']['value'];
 		$d .= sprintf('<td align=right>%s', priority2a($v['priority']['value']));
 		$d .= sprintf('<td align=left>%s', state2a($v['state']['value']));
 		return $d;
@@ -149,12 +149,12 @@
 			$d .= sprintf('<th align=right>%s', b2a($v['aggregate']['total uploaded']));
 			$d .= sprintf('<th align=right>%s', b2a($v['aggregate']['down rate']));
 			$d .= sprintf('<th align=right>%s', b2a($v['aggregate']['up rate']));
-			$d .= sprintf('<th align=right>%s', nz($v['aggregate']['incomplete']));
-			if ($v['aggregate']['incomplete total'])
-				$d .= ' / ' . $v['aggregate']['incomplete total'];
-			$d .= sprintf('<th align=right>%s', nz($v['aggregate']['complete']));
-			if ($v['aggregate']['complete total'])
-				$d .= ' / ' . $v['aggregate']['complete total'];
+			$d .= sprintf('<th align=right>');
+			if ($v['aggregate']['incomplete'] || $v['aggregate']['incomplete total'])
+				$d .= $v['aggregate']['incomplete'] . ' / ' . $v['aggregate']['incomplete total'];
+			$d .= sprintf('<th align=right>');
+			if ($v['aggregate']['complete'] || $v['aggregate']['complete total'])
+				$d .= $v['aggregate']['complete'] . ' / ' . $v['aggregate']['complete total'];
 			$d .= '<th>';
 			$d .= '<th>';
 		}
