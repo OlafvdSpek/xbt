b55b5a2398619bb7b411a9094f9d5e78a2b881b3
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2005-06-12 21:27:42 +0000

    no message

--- xbt/misc/windows/browse_for_directory.cpp
+++ xbt/misc/windows/browse_for_directory.cpp
@@ -1,6 +1,13 @@
 #include "stdafx.h"
 #include "browse_for_directory.h"
 
+int CALLBACK BrowseCallbackProc(HWND hwnd, UINT uMsg, LPARAM lParam, LPARAM lpData)
+{
+	if (uMsg == BFFM_INITIALIZED)
+		SendMessage(hwnd, BFFM_SETSELECTION, true, lpData);
+	return 0;
+}
+
 int browse_for_directory(HWND hWnd, const string& title, string& directory)
 {
 	BROWSEINFO bi;
@@ -8,6 +15,8 @@ int browse_for_directory(HWND hWnd, const string& title, string& directory)
 	bi.hwndOwner = hWnd;
 	bi.lpszTitle = title.c_str();
 	bi.ulFlags = BIF_NEWDIALOGSTYLE | BIF_RETURNONLYFSDIRS;
+	bi.lpfn = BrowseCallbackProc;
+	bi.lParam = reinterpret_cast<LPARAM>(directory.c_str());
 	ITEMIDLIST* idl = SHBrowseForFolder(&bi);
 	if (!idl)
 		return 1;
