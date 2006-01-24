123151a4f9b1a9fbe25ca14c9538a4f2b80415df
olafvdspek <olafvdspek@a4ef9278-c6d2-effe-6dce-f9b4ebbbbc1a>
2006-01-24 07:51:21 +0000

    no message

---
+++ trunk/xbt/Peert Gateway/XBT Peert Gateway.cpp
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+#include "windows/nt_service.h"
+#include "server.h"
+
+const char* g_service_name = "XBT Peert Gateway";
+
+int main1()
+{
+	return Cserver().run();
+}
+
+#ifdef WIN32
+static SERVICE_STATUS g_service_status;
+static SERVICE_STATUS_HANDLE gh_service_status;
+
+void WINAPI nt_service_handler(DWORD op)
+{
+	switch (op)
+	{
+	case SERVICE_CONTROL_STOP:
+		g_service_status.dwCurrentState = SERVICE_STOP_PENDING;
+		SetServiceStatus(gh_service_status, &g_service_status);
+		Cserver::term();
+		break;
+	}
+	SetServiceStatus(gh_service_status, &g_service_status);
+}
+
+void WINAPI nt_service_main(DWORD argc, LPTSTR* argv)
+{
+	g_service_status.dwCheckPoint = 0;
+	g_service_status.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+	g_service_status.dwCurrentState = SERVICE_START_PENDING;
+	g_service_status.dwServiceSpecificExitCode = 0;
+	g_service_status.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+	g_service_status.dwWaitHint = 0;
+	g_service_status.dwWin32ExitCode = NO_ERROR;
+	if (!(gh_service_status = RegisterServiceCtrlHandler(g_service_name, nt_service_handler)))
+		return;
+	SetServiceStatus(gh_service_status, &g_service_status);
+	g_service_status.dwCurrentState = SERVICE_RUNNING;
+	SetServiceStatus(gh_service_status, &g_service_status);
+	main1();
+	g_service_status.dwCurrentState = SERVICE_STOPPED;
+	SetServiceStatus(gh_service_status, &g_service_status);
+}
+#endif
+
+int main(int argc, char* argv[])
+{
+	curl_global_init(CURL_GLOBAL_ALL);
+#ifdef WIN32
+	if (argc >= 2)
+	{
+		if (!strcmp(argv[1], "--install"))
+		{
+			if (nt_service_install(g_service_name))
+				return cerr << "Failed to install service " << g_service_name << "." << endl, 1;
+			cout << "Service " << g_service_name << " has been installed." << endl;
+			return 0;
+		}
+		else if (!strcmp(argv[1], "--uninstall"))
+		{
+			if (nt_service_uninstall(g_service_name))
+				return cerr << "Failed to uninstall service " << g_service_name << "." << endl, 1;
+			cout << "Service " << g_service_name << " has been uninstalled." << endl;
+			return 0;
+		}
+		else
+			return 1;
+	}
+	WSADATA wsadata;
+	if (WSAStartup(MAKEWORD(2, 0), &wsadata))
+		return cerr << "Unable to start WSA" << endl, 1;
+#ifdef NDEBUG
+	SERVICE_TABLE_ENTRY st[] = 
+	{
+		{ "", nt_service_main },
+		{ NULL, NULL }
+	};
+	if (StartServiceCtrlDispatcher(st))
+		return 0;
+	if (GetLastError() != ERROR_CALL_NOT_IMPLEMENTED
+		&& GetLastError() != ERROR_FAILED_SERVICE_CONTROLLER_CONNECT)		
+		return 1;
+#endif
+#endif
+	return main1();
+}
---
+++ trunk/xbt/Peert Gateway/connection_handler_http_server.cpp
@@ -0,0 +1,251 @@
+#include "stdafx.h"
+#include "connection_handler_http_server.h"
+
+#include "connection.h"
+#include "server.h"
+
+Cconnection_handler_http_server::Cconnection_handler_http_server()
+{
+}
+
+Cconnection_handler_http_server::~Cconnection_handler_http_server()
+{
+}
+
+int Cconnection_handler_http_server::post_recv(Cconnection* con)
+{
+	string s(con->read_b().r(), con->read_b().cb_r());
+	if (s.find("\r\n\r\n") != string::npos)
+	{
+		int i = s.find("\r\n");
+		read(con, s.substr(0, i));
+		return 1;
+	}
+	return 0;
+}
+
+int Cconnection_handler_http_server::post_send(Cconnection* con)
+{
+	return 0;
+}
+
+static int send(const Cbvalue& v, Cbvalue* v1 = NULL)
+{
+	Csocket a;
+	if (a.open(SOCK_STREAM, true) == INVALID_SOCKET)
+		return 1;
+	if (a.connect(htonl(INADDR_LOOPBACK), htons(6879)))
+		return 1;
+	char d0[5];
+	Cvirtual_binary d1 = v.read();
+	*reinterpret_cast<__int32*>(d0) = htonl(d1.size() + 1);
+	d0[4] = bti_bvalue;
+	if (a.send(d0, 5) != 5)
+		return 1;
+	if (a.send(d1, d1.size()) != d1.size())
+		return 1;
+	const int cb_d = 1 << 20;
+	vector<char> d(cb_d);
+	char* w = &d.front();
+	int r;
+	while (w - &d.front() != cb_d && (r = a.recv(w, &d.front() + cb_d - w)))
+	{
+		if (r == SOCKET_ERROR)
+			break;
+		w += r;
+		if (w - &d.front() >= 4 && w - &d.front() >= ntohl(*reinterpret_cast<__int32*>(&d.front())))
+			return v1 ? v1->write(&d.front() + 5, ntohl(*reinterpret_cast<__int32*>(&d.front())) - 1) : 0;
+	}
+	return 1;
+}
+
+void Cconnection_handler_http_server::read(Cconnection* con, const string& v)
+{
+#if 1 // ndef NDEBUG
+	cout << v << endl;
+#endif
+	string h = "HTTP/1.0 200 OK\r\n";
+	string s;
+	h += "Content-Type: text/plain; charset=utf-8\r\n";
+	int a = v.find(' ');
+	if (a == string::npos)
+		return;
+	string request_method = v.substr(0, a);
+	string script_name;
+	string info_hash;
+	string pass_id;
+	int priority = 0;
+	string session_id;
+	int state = 0;
+	string url;
+	a++;
+	int b = v.find_first_of(" ?", a);
+	if (b != string::npos)
+	{
+		script_name = v.substr(a, b - a);
+		if (v[b] == '?')
+		{
+			a = b + 1;
+			int b = v.find(' ', a);
+			if (b == string::npos)
+				return;
+			while (a < b)
+			{
+				int c = v.find('=', a);
+				if (c++ == string::npos)
+					break;
+				int d = v.find_first_of(" &;", c);
+				assert(d != string::npos);
+				string name = v.substr(a, c - a - 1);
+				string value = uri_decode(v.substr(c, d - c));
+				if (name == "info_hash")
+					info_hash = hex_decode(value);
+				else if (name == "pass_id")
+					pass_id = value;
+				else if (name == "priority")
+					priority = atoi(value.c_str());
+				else if (name == "session_id")
+					session_id = value;
+				else if (name == "state")
+					state = atoi(value.c_str());
+				else if (name == "url")
+					url = value;
+				a = d + 1;
+			}
+		}
+	}
+	if (script_name == "/accept")
+	{
+		s += "passid = '" + pass_id + "'\n";
+		s += "sessionid = '" + session_id + "'\n";
+		s += "alert('ACCEPT');\n";
+		s += "alert('DECLINED');\n";
+	}
+	else if (script_name == "/close")
+	{
+		Cbvalue v;
+		v.d(bts_action, bts_close_torrent);
+		v.d(bts_hash, info_hash);
+		if (::send(v))
+			s += "_xbt.callback_error();\n";
+		else
+		{
+			s += "_xbt.callback_close('" + hex_encode(info_hash) + "');\n";
+		}
+	}
+	else if (script_name == "/delete")
+	{
+		Cbvalue v;
+		v.d(bts_action, bts_erase_torrent);
+		v.d(bts_hash, info_hash);
+		if (::send(v))
+			s += "_xbt.callback_error();\n";
+		else
+		{
+			s += "_xbt.callback_delete('" + hex_encode(info_hash) + "');\n";
+		}
+	}
+	else if (script_name == "/make")
+	{
+	}
+	else if (script_name == "/open")
+	{
+		string d;
+		con->server()->http_get(url, d);
+		Cbvalue v;
+		v.d(bts_action, bts_open_torrent);
+		v.d(bts_torrent, d);
+		if (::send(v))
+			s += "_xbt.callback_error();\n";
+		else
+		{
+			Cbvalue d1(d.c_str(), d.size());
+			s += "_xbt.callback_open('" + hex_encode(compute_sha1(d1.d(bts_info).read())) + "', '" + d1.d(bts_info).d(bts_name).s() + "', " + n(d1.d(bts_info).d(bts_length).i()) + ");\n";
+		}
+	}
+	else if (script_name == "/options")
+	{
+	}
+	else if (script_name == "/port")
+	{
+		s += "_xbt.callback_port(80);\n";
+	}
+	else if (script_name == "/priority")
+	{
+		Cbvalue v;
+		v.d(bts_action, bts_set_priority);
+		v.d(bts_hash, info_hash);
+		v.d(bts_priority, priority);
+		if (::send(v))
+			s += "_xbt.callback_error();\n";
+		else
+		{
+			s += "_xbt.callback_priority('" + hex_encode(info_hash) + "', " + n(state) + ");\n";
+		}		
+	}
+	else if (script_name == "/session")
+	{
+		s += "_xbt.callback_session();\n";
+	}
+	else if (script_name == "/state")
+	{
+		Cbvalue v;
+		v.d(bts_action, bts_set_state);
+		v.d(bts_hash, info_hash);
+		v.d(bts_state, state);
+		if (::send(v))
+			s += "_xbt.callback_error();\n";
+		else
+		{
+			s += "_xbt.callback_state('" + hex_encode(info_hash) + "', " + n(state) + ");\n";
+		}		
+	}
+	else if (script_name == "/status")
+	{
+		Cbvalue v, d;
+		v.d(bts_action, bts_get_status);
+		if (::send(v, &d))
+			s += "_xbt.callback_error();\n";
+		else
+		{
+			s += "_xbt.callback_beginUpdate();\n";
+			const Cbvalue& files = d.d(bts_files);
+			for (Cbvalue::t_map::const_iterator i = files.d().begin(); i != files.d().end(); i++)
+			{
+				s += "_xbt.callback_update('" + hex_encode(i->first) + "', " 
+					+ "'" + js_encode(i->second.d(bts_name).s()) + "', " 
+					+ n(i->second.d(bts_left).i()) + ", " 
+					+ n(i->second.d(bts_size).i()) + ", " 
+					+ n(i->second.d(bts_total_downloaded).i()) + ", " 
+					+ n(i->second.d(bts_total_uploaded).i()) + ", " 
+					+ n(i->second.d(bts_down_rate).i()) + ", " 
+					+ n(i->second.d(bts_up_rate).i()) + ", " 
+					+ n(i->second.d(bts_incomplete).i()) + ", " 
+					+ n(i->second.d(bts_incomplete_total).i()) + ", " 
+					+ n(i->second.d(bts_complete).i()) + ", " 
+					+ n(i->second.d(bts_complete_total).i()) + ", " 
+					+ n(i->second.d(bts_priority).i()) + ", " 
+					+ n(i->second.d(bts_state).i()) + ", " 
+					+ n(i->second.d(bts_completed_at).i()) + ", " 
+					+ n(i->second.d(bts_started_at).i()) + ");\n";
+			}
+			s += "_xbt.callback_endUpdate();\n";
+		}
+	}
+	else
+	{
+		s += "<title>Hello World!</title>";
+		s += "<h1>Hello World!</h1>";
+	}
+	h += "\r\n";
+	Cvirtual_binary d;
+	memcpy(d.write_start(h.size() + s.size()), h.c_str(), h.size());
+	memcpy(d.data_edit() + h.size(), s.c_str(), s.size());
+	int r = con->s().send(d, d.size());
+	if (r == SOCKET_ERROR)
+		cerr << "send failed: " << Csocket::error2a(WSAGetLastError()) << endl;
+	else if (r != d.size())
+	{
+		cerr << "send failed: partial send" << endl;
+	}
+}
