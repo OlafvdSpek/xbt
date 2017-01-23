e4db47ab03c03304c4c3038798a98da8ff1c9baa
Olaf van der Spek <olafvdspek@gmail.com>
2017-01-23 22:29:12 +0100

    -

--- misc/xbt/string_view.h
+++ misc/xbt/string_view.h
@@ -1,3 +1,6 @@
+#pragma once
+
+#include <boost/lexical_cast.hpp>
 #include <boost/utility/string_ref.hpp>
 #include <string>
 
@@ -19,4 +22,34 @@ inline std::string& operator<<(std::string& a, std::string_view b)
 inline std::string& operator<<(std::string& a, long long b)
 {
 	return a += std::to_string(b);
+}
+
+inline float to_float(std::string_view v)
+{
+	// return boost::convert<float>(v, boost::cnv::strtol(), 0.0f);
+	if (v.empty())
+		return 0;
+	try
+	{
+		return boost::lexical_cast<float>(v);
+	}
+	catch (boost::bad_lexical_cast&)
+	{
+	}
+	return 0;
+}
+
+inline long long to_int(std::string_view v)
+{
+	// return boost::convert<long long>(v, boost::cnv::strtol(), 0);
+	if (v.empty())
+		return 0;
+	try
+	{
+		return boost::lexical_cast<long long>(v);
+	}
+	catch (boost::bad_lexical_cast&)
+	{
+	}
+	return 0;
 }
